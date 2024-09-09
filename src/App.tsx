505c128572233a6ad7cd5c32ab0ad5ceb0d4fab8
mgmichaela <mgmichaelaguzmicka@gmail.com>
2024-09-09 13:43:37 +0200

    remove warning component

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Games from "./components/Games";
 import GlobalStyles from "./components/GlobalStyles";
 import { Outlet } from "react-router-dom";
 import Nav from "./components/Nav";
-import Warning from "./components/Warning";
 
 const App = () => {
   const [showDefaultGames, setShowDefaultGames] = useState<boolean>(true);
-  const [isAgeConfirmed, setIsAgeConfirmed] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const ageConfirmed = localStorage.getItem("isAgeConfirmed");
-
-    if (ageConfirmed === "true") {
-      setIsAgeConfirmed(true);
-    }
-
-    setLoading(false);
-  }, []);
-
-  if (loading) {
-    return null;
-  }
 
   return (
     <div>
       <GlobalStyles />
-      {isAgeConfirmed ? (
-        <>
-          <Nav setShowDefaultGames={setShowDefaultGames} />
-          <Games showDefaultGames={showDefaultGames} />
-        </>
-      ) : (
-        <Warning setIsAgeConfirmed={setIsAgeConfirmed} />
-      )}
+      <Nav setShowDefaultGames={setShowDefaultGames} />
+      <Games showDefaultGames={showDefaultGames} />
       <Outlet />
     </div>
   );
